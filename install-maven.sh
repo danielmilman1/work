7340b2b063a1c0a4b9f07b8fed45abb21813f33c
danielmilman1 <116508449+danielmilman1@users.noreply.github.com>
2023-11-14 12:09:46 +0200

    Update install-maven.sh

--- install-maven.sh
+++ install-maven.sh
@@ -1,13 +1,15 @@
-#install maven client
+#install java
 wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
 tar -xvf openjdk-13.0.1_linux-x64_bin.tar.gz
 mv jdk-13.0.1 /opt/
 
+#install maven client
+wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xvf apache-maven-3.6.3-bin.tar.gz
+mv apache-maven-3.6.3 /opt/
+
 #export directories for global access from any directory
-echo "JAVA_HOME='/opt/jdk-13.0.1'
-PATH="$JAVA_HOME/bin:$PATH"
-JAVA_HOME='/opt/jdk-13.0.1'
-PATH="$JAVA_HOME/bin:$PATH"
+echo "PATH=/opt/jdk-13.0.1/bin:/opt/apache-maven-3.6.3/bin:$PATH
 export PATH" >> ~/.bashrc
 
 #source file to initiate new config right now
